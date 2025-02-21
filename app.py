d8b2dfa356c4bff28c85bc7940ffa1daee4158cd
MichelleQM <148285778+MichelleQM@users.noreply.github.com>
2025-02-21 17:48:28 -0600

    Clase 21 feb

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import mysql.connector
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="mwqm1961",
+        database="prueba1"
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM studens")
+    students = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('index.html', students=students)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
